801ee329c91ae8e32b31ecf2b03aa7966eb529f5
leungjunchiang <110956472+leungjunchiang@users.noreply.github.com>
2026-08-25 20:23:30 +0800

    fix: align cloudbase reaction relay

--- relay/cloudbase-function/index.js
+++ relay/cloudbase-function/index.js
@@ -5,7 +5,7 @@
 
 const RPC_ALLOWLIST = new Set([
   "lili_add_buddy_by_code", "lili_lookup_buddy_by_code", "lili_buddy_requests", "lili_respond_buddy", "lili_cancel_buddy_request", "lili_remove_buddy", "lili_create_room", "lili_join_room",
-  "lili_send_visit", "lili_respond_visit", "lili_send_taunt", "lili_taunt_state", "lili_dashboard", "lili_room_dashboard",
+  "lili_send_visit", "lili_respond_visit", "lili_send_taunt", "lili_taunt_state", "lili_send_encouragement", "lili_encouragement_state", "lili_reaction_state", "lili_dashboard", "lili_room_dashboard",
   "lili_record_room_event", "lili_send_interaction", "lili_create_cake_share", "lili_set_room_goal", "lili_leave_room",
   "lili_set_room_schedule", "lili_set_room_challenge", "lili_set_buddy_subscription",
   "lili_room_room_rituals",
@@ -17,7 +17,7 @@ const ROUTE_TO_RPC = new Map([
   ["/buddies/request", "lili_add_buddy_by_code"], ["/buddies/lookup", "lili_lookup_buddy_by_code"], ["/buddies/requests", "lili_buddy_requests"], ["/buddies/accept", "lili_respond_buddy"], ["/buddies/cancel", "lili_cancel_buddy_request"],
   ["/buddies/remove", "lili_remove_buddy"], ["/buddies/subscription", "lili_set_buddy_subscription"],
   ["/visits/send", "lili_send_visit"], ["/visits/accept", "lili_respond_visit"],
-  ["/buddies/taunt", "lili_send_taunt"], ["/buddies/taunt-state", "lili_taunt_state"],
+  ["/buddies/taunt", "lili_send_taunt"], ["/buddies/taunt-state", "lili_taunt_state"], ["/buddies/encouragement", "lili_send_encouragement"], ["/buddies/encouragement-state", "lili_encouragement_state"], ["/buddies/reaction-state", "lili_reaction_state"],
   ["/rooms/create", "lili_create_room"], ["/rooms/join", "lili_join_room"],
   ["/rooms/goal", "lili_set_room_goal"], ["/rooms/schedule", "lili_set_room_schedule"],
   ["/rooms/challenge", "lili_set_room_challenge"], ["/rooms/leave", "lili_leave_room"],
